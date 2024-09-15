8edc89d0d9b9ce6ddfcb2d677768a216417d654e
Min Lwin <lwin.zawmin@gmail.com>
2024-09-15 21:47:46 +0630

    Using Maven Repository Package Version 0.0.1-SNAPSHOT

--- src/main/java/com/jdc/maven/Application.java
+++ src/main/java/com/jdc/maven/Application.java
@@ -2,8 +2,10 @@ package com.jdc.maven;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
+@ComponentScan(basePackages = "com.jdc.commons")
 public class Application {
 
 	public static void main(String[] args) {
--- src/test/java/com/jdc/maven/ApplicationTest.java
+++ src/test/java/com/jdc/maven/ApplicationTest.java
@@ -1,13 +1,21 @@
 package com.jdc.maven;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import com.jdc.commons.services.HelloService;
 
 @SpringBootTest
 public class ApplicationTest {
+	
+	@Autowired
+	private HelloService service;
 
 	@Test
 	void test() {
-		
+		assertEquals("Hello Maven Package Work flow.", service.message());
 	}
 }
