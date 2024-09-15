abfb6a5a2dbe444f216456baf4e467b8c200cfaa
Min Lwin <lwin.zawmin@gmail.com>
2024-09-16 00:35:26 +0630

    Update Workflow file

--- src/main/java/com/jdc/maven/Application.java
+++ src/main/java/com/jdc/maven/Application.java
@@ -2,10 +2,8 @@ package com.jdc.maven;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
-@ComponentScan(basePackages = "com.jdc.commons")
 public class Application {
 
 	public static void main(String[] args) {
