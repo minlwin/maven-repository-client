c44ec348c4e3d711f050072afe53437595533e26
Min Lwin <lwin.zawmin@gmail.com>
2024-09-15 18:09:52 +0630

    First Commit

---
+++ src/test/java/com/jdc/maven/ApplicationTest.java
@@ -0,0 +1,13 @@
+package com.jdc.maven;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+public class ApplicationTest {
+
+	@Test
+	void test() {
+		
+	}
+}
